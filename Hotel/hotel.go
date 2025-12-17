cb9bcf00c4b49f7cd672f91e188cdf57453d021b
Altusha4 <240782@astanait.edu.kz>
2025-12-17 15:29:35 +0600

    Add CheckIn method

--- Hotel/hotel.go
+++ Hotel/hotel.go
@@ -25,3 +25,17 @@ func (h *Hotel) AddRoom(number string, roomType string, price float64) {
 		IsOccupied:    false,
 	}
 }
+
+func (h *Hotel) CheckIn(number string) {
+	room, exists := h.Rooms[number]
+	if !exists {
+		return
+	}
+
+	if room.IsOccupied {
+		return
+	}
+
+	room.IsOccupied = true
+	h.Rooms[number] = room
+}
