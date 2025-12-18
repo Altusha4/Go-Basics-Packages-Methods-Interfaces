d252be2cc04be099228969d8755648bcbd7be953
Altusha4 <240782@astanait.edu.kz>
2025-12-18 12:54:07 +0600

    Add hotel demo in main

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"Assignment1/Hotel"
+)
+
+func main() {
+	hotel := Hotel.NewHotel()
+
+	hotel.AddRoom("101", "Single", 100)
+	hotel.AddRoom("102", "Double", 150)
+
+	hotel.CheckIn("101")
+
+	hotel.ListVacantRooms()
+}
