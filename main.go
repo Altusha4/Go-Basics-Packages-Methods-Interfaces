a019c0145498753fed2d3061b016906e4c8ee894
Altusha4 <240782@astanait.edu.kz>
2025-12-18 13:18:15 +0600

    Update employee demo using interface slice and loop

--- main.go
+++ main.go
@@ -11,10 +11,23 @@ func main() {
 			MonthlySalary: 300000,
 			BonusRate:     0.1,
 		},
+		Employee.PartTime{
+			HourlyRate:  2000,
+			HoursWorked: 80,
+		},
+		Employee.Contractor{
+			ProjectRate:       50000,
+			ProjectsCompleted: 4,
+		},
+		Employee.Intern{
+			DailyRate:  8000,
+			DaysWorked: 22,
+		},
 	}
 
 	for _, e := range employees {
 		fmt.Println("Salary:", e.CalculateSalary())
 		fmt.Println("Work hours:", e.GetWorkHours())
+		fmt.Println("---")
 	}
 }
