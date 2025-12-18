2369e33eafb21073e8c9d37cea11ababf6f7f014
Altusha4 <240782@astanait.edu.kz>
2025-12-18 13:13:06 +0600

    Add PartTime salary and bonus methods

--- Employee/employee.go
+++ Employee/employee.go
@@ -36,3 +36,15 @@ func (f FullTime) CalculateBonus() float64 {
 func (f FullTime) GetWorkHours() int {
 	return 160
 }
+
+func (p PartTime) CalculateSalary() float64 {
+	return float64(p.HoursWorked) * p.HourlyRate
+}
+
+func (p PartTime) CalculateBonus() float64 {
+	return 0
+}
+
+func (p PartTime) GetWorkHours() int {
+	return p.HoursWorked
+}
